2b593f13590358148811d1300914d7a42acc1583
Armin Kemura <52348946+brinobre@users.noreply.github.com>
2020-08-27 15:56:26 +0200

    modified scene
    added more XR controllers and examples

---
+++ Assets/Scripts/CubeBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehaviour : MonoBehaviour
+{
+    public void GrowCube()
+    {
+        transform.localScale *= 1.1f;
+    }
+}
